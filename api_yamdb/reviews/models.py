44a0c488b6cea2e41a967bc8f4760aa8c9d6e947
Aleksandr Kovalev <ReiMowakari@yandex.ru>
2024-07-30 19:55:37 +0300

    add checking range in score in Review and small fix models

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,11 +1,14 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
-class Reviews(models.Model):
+class Review(models.Model):
     """Модель для отзывов."""
     text = models.TextField('Содержимое отзыва')
     #author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Автор')
-    score = models.PositiveSmallIntegerField('Оценка', default=1)
+    score = models.IntegerField('Оценка', default=1, validators=[
+        MinValueValidator(1), MaxValueValidator(10)
+    ])
     #title = models.ForeignKey(Title,on_delete=models.CASCADE, verbose_name='Произведение')
     pub_date = models.DateTimeField(
         'Дата публикации',
@@ -17,15 +20,22 @@ class Reviews(models.Model):
         verbose_name_plural = 'Отзывы'
         default_related_name = 'reviews'
         ordering = ['-pub_date']
-        #constraints = models.UniqueConstraint(fields=['title', 'author'],name='unique_author_title')
+        # Ограничения на то, что можно оставить только один отзыв к произведению
+        # от одного автора, а также ограничение на проверку оценки в диапазоне от 1 до 10.
+        #constraints = [
+        # models.UniqueConstraint(fields=['title', 'author'],
+        # name='unique_author_title'),
+        # models.CheckConstraint(check=(F('score') >= 1) & (F('score') <= 10),
+        # name='check_range_score')
+        # ]
 
 
-class Comments(models.Model):
+class Comment(models.Model):
     """Модель для комментариев."""
     text = models.TextField('Содержимое комментария')
     #author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Автор')
     review = models.ForeignKey(
-        Reviews,
+        Review,
         on_delete=models.CASCADE,
     )
     pub_date = models.DateTimeField(
