58e295ab464cc746b834396ffd537622a2b8c531
Александр Келесидис <Sashagvke@gmail.com>
2024-07-31 08:11:43 +0300

    Created custom model of users.

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, Group
+from django.core.exceptions import ObjectDoesNotExist
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    groups = None
+
+    @staticmethod
+    def get_default_role():
+        try:
+            return Group.objects.get(name='user').id
+        except ObjectDoesNotExist:
+            return None
+
+    role = models.ForeignKey(
+        Group,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        verbose_name='роль',
+        related_name='users',
+        default=get_default_role(),
+        max_length=16,
+        help_text='Роль пользователя, определяющая доступ к ресурсам проекта.'
+    )
+    bio = models.TextField(blank=True, verbose_name='биография')
+
+    class Meta:
+        verbose_name = 'пользователь'
+        verbose_name_plural = 'Пользователи'
+        ordering = ('username', 'date_joined')
+
+    def __str__(self):
+        return self.username
+
+    # TODO Проверку существования объекта модели Group.
