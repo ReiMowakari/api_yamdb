8b98bf30a18bcea4b356ee4de4c0d08d9f565d39
Александр Келесидис <Sashagvke@gmail.com>
2024-07-31 19:05:35 +0300

    Внесены правки в модель CustomUser согласно комментариям PR.

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,39 +1,55 @@
 from django.db import models
+from django.core import validators
 from django.contrib.auth.models import AbstractUser, Group
-from django.core.exceptions import ObjectDoesNotExist
+
+
+def get_default_role():
+    """Функция возвращает id роли user из модели Group."""
+    group, created = Group.objects.get_or_create(name='user')
+    return group.pk
 
 
 class CustomUser(AbstractUser):
-    """Кастомная модель юзеров. Имеет one-to-many на модель Group
-    для разаграничения доступа юзерам.
     """
+    Кастомная модель юзеров.
+
+    Имеет one-to-many на модель Group для разаграничения доступа юзерам.
+    """
+
     groups = None
-
-    @staticmethod
-    def get_default_role():
-        """Классовый метод возвращающий id роли user из модели Group."""
-        try:
-            return Group.objects.get(name='user').id
-        except ObjectDoesNotExist:
-            return None
-
     role = models.ForeignKey(
         Group,
         on_delete=models.SET_NULL,
-        null=True,
         blank=True,
+        null=True,
         verbose_name='роль',
-        related_name='users',
-        default=get_default_role(),
         max_length=16,
-        help_text='Роль пользователя, определяющая доступ к ресурсам проекта.'
+        help_text='Роль пользователя, определяющая доступ к ресурсам проекта'
     )
-    bio = models.TextField(blank=True, verbose_name='биография', max_length=256)
+    email = models.EmailField(
+        max_length=254,
+        verbose_name='Электронный адрес',
+        unique=True,
+        validators=[validators.validate_email]
+    )
+    confirmation_code = models.CharField(
+        'Код подтверждения', max_length=50, blank=True, null=True
+    )
+    bio = models.TextField(
+        blank=True, verbose_name='О себе',
+    )
 
     class Meta:
         verbose_name = 'пользователь'
         verbose_name_plural = 'Пользователи'
-        ordering = ('username', 'date_joined')
+        ordering = ('username', '-date_joined')
+        default_related_name = 'users'
+
+    def save(self, *args, **kwargs):
+        self.clean()
+        if not self.role:
+            self.role = get_default_role()
+        super(CustomUser, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.username
