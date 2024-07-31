e07a4a47f857a48458663beca0fe9fd76f333951
Aleksandr Kovalev <ReiMowakari@yandex.ru>
2024-07-31 18:10:57 +0300

    small fix on Review model

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,17 +1,22 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class Review(models.Model):
     """Модель для отзывов."""
 
     text = models.TextField('Содержимое отзыва')
-    #author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Автор')
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, verbose_name='Автор')
     score = models.PositiveSmallIntegerField(
         'Оценка', default=1, validators=[
             MinValueValidator(1, message='Оценка не может быть меньше 1'),
             MaxValueValidator(10, message='Оценка не может быть больше 10')])
-    #title = models.ForeignKey(Title,on_delete=models.CASCADE, verbose_name='Произведение')
+    title = models.ForeignKey(
+        Title, on_delete=models.CASCADE, verbose_name='Произведение')
     pub_date = models.DateTimeField(
         'Дата публикации',
         auto_now_add=True
@@ -19,23 +24,22 @@ class Review(models.Model):
 
     class Meta:
         verbose_name = 'отзыв'
-        verbose_name_plural = 'отзывы'
+        verbose_name_plural = 'Отзывы'
         default_related_name = 'reviews'
         ordering = ['-pub_date']
         # Ограничения на то, что можно оставить только один отзыв к произведению
         # от одного автора, а также ограничение на проверку оценки в диапазоне от 1 до 10.
-        #constraints = [
-        # models.UniqueConstraint(fields=['title', 'author'],
-        # name='unique_author_title'),
-        # models.CheckConstraint(check=(F('score') >= 1) & (F('score') <= 10),
-        # name='check_range_score')
-        # ]
+        constraints = [
+            models.UniqueConstraint(fields=['title', 'author'],
+                                    name='unique_review_author_title'),
+        ]
 
 
 class Comment(models.Model):
     """Модель для комментариев."""
     text = models.TextField('Содержимое комментария')
-    #author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Автор')
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, verbose_name='Автор')
     review = models.ForeignKey(
         Review,
         on_delete=models.CASCADE,
@@ -47,6 +51,6 @@ class Comment(models.Model):
 
     class Meta:
         verbose_name = 'комментарий'
-        verbose_name_plural = 'комментарии'
+        verbose_name_plural = 'Комментарии'
         default_related_name = 'comments'
         ordering = ['-pub_date']
